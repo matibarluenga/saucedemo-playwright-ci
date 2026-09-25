e93e855c6dd90c847da817a126d558b7452b765c
matibarluenga <matibarluenga@gmail.com>
2026-09-25 17:07:42 -0300

    e2e login and purchase: in progress

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -41,3 +41,36 @@ test("invalid password", async ({ page }) => {
   await expect(page).not.toHaveURL("/inventory.html");
   await expect(page.getByTestId("error")).toContainText("do not match");
 });
+
+test("e2e login and complete a purchase", async ({ page }) => {
+  //login
+  await page.goto("/");
+  await page.getByTestId("username").fill("standard_user");
+  await page.getByTestId("password").fill("secret_sauce");
+  await page.getByTestId("login-button").click();
+  await expect(page).toHaveURL("/inventory.html");
+  await expect(page.getByTestId("title")).toHaveText("Products");
+
+  //validate that no product is in the cart
+  const cartBadge = page.getByTestId("shopping-cart-badge");
+  if ((await cartBadge.count()) > 0) {
+    await page.getByTestId("shopping-cart-link").click();
+    const removeButtons = page.locator(".cart_button");
+    while ((await removeButtons.count()) > 0) {
+      await removeButtons.first().click();
+    }
+    await page.getByTestId("continue-shopping").click();
+  }
+  await expect(cartBadge).toHaveCount(0);
+
+  //add a product to the cart
+  await page.getByTestId("add-to-cart-sauce-labs-backpack").click();
+  await expect(
+    page
+      .getByTestId("inventory-item-description")
+      .filter({ hasText: "Sauce Labs Backpack" })
+      .getByTestId("remove-sauce-labs-backpack"),
+  ).toBeVisible();
+
+  //check the added products
+});
